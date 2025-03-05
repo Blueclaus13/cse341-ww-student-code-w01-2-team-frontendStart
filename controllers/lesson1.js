4576b93cfbea0b3248bedf45b1a3dc9b56545f31
Claudia Madrid <71229725+Blueclaus13@users.noreply.github.com>
2025-03-05 09:57:21 -0600

    separation of bussiness logic and routes

---
+++ controllers/lesson1.js
@@ -0,0 +1,7 @@
+const professional = (req, res)=>{
+    res.send('Hello World, This is profile router');
+};
+
+module.exports ={
+    professional
+};
--- server.js
+++ server.js
@@ -1,23 +1,17 @@
+const PORT = process.env.PORT || 8080;
 //Initialize App
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const router = express.Router();
-const PORT = process.env.PORT || 8080;
- 
 //Add middleware before routes
 app.use(bodyParser.json());
-
-router.get('/professional', (req, res) =>{
-  res.send('Hello World, This is profile router');
-    });
     
 
 app.use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
-app.use('/', router);
+app.use('/', require('./routes/index.js'));
  
 app.listen(PORT, () => {
   console.log('Web Server is listening at port ' + (PORT));
